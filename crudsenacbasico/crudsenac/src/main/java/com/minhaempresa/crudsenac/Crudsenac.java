6c78de522a8cd7406f58b8f657ca613935b3109e
Guiba <guilherme.ssoares13@hotmail.com>
2023-10-16 21:50:27 -0300

    Cód_Final

---
+++ crudsenacbasico/crudsenac/src/main/java/com/minhaempresa/crudsenac/Crudsenac.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.minhaempresa.crudsenac;
+
+import com.minhaempresa.crudsenac.views.ViewConsultaNotas;
+
+/**
+ *
+ * @author ftfer
+ */
+public class Crudsenac {
+
+    public static void main(String[] args) {
+        ViewConsultaNotas janelaPrincipal = new ViewConsultaNotas();
+        janelaPrincipal.setVisible(true);
+    }
+}
